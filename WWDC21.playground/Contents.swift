9c4e85fac84967693312b9ad5bedbe15c5235cbe
Dhaval B <pythondsb@gmail.com>
2021-04-03 12:11:48 -0700

    Actionless scroll

--- WWDC21.playground/Contents.swift
+++ WWDC21.playground/Contents.swift
@@ -1,9 +1,9 @@
 /*:
  # WWDC 2021 Playground Submission
-Credits to Vecteezy.com for gas can images.
 
- Description:
- 
+ This vertical scrolling experience is about a vehicle low on gas driving on an abandoned road. Collect gas along the way to be able to make it to the city.
+- - -
+  `Credits to Vecteezy.com for gas can images.`
  */
 
 loadPrgm(true)
