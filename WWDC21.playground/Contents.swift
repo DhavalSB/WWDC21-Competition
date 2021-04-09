d5054a2c2babdff73752f9e70a75bbc930c5edc8
Dhaval B <pythondsb@gmail.com>
2021-04-09 13:03:47 -0700

    added turn block.

--- WWDC21.playground/Contents.swift
+++ WWDC21.playground/Contents.swift
@@ -1,7 +1,7 @@
 /*:
  # WWDC 2021 Playground Submission
 
- This vertical scrolling experience is about a vehicle low on gas driving on an abandoned road. Collect gas along the way to be able to make it to the city.
+ This vertical scrolling experience is about a vehicle low on gas driving on an abandoned road. Avoid obstacles and collect gas along the way to be able to make it to the city successfully.
 - - -
   `Credits to Vecteezy.com for gas can and gas meter images.`
  */
