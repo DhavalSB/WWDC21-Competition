74ef65dc2d7c925f306e30ce39d68e8fb30017b0
Dhaval B <pythondsb@gmail.com>
2021-04-10 14:29:53 -0700

    Game finished

--- WWDC21.playground/Contents.swift
+++ WWDC21.playground/Contents.swift
@@ -1,9 +1,27 @@
 /*:
- # WWDC 2021 Playground Submission
+ # WWDC 2021 Playground
 
- This vertical scrolling experience is about a vehicle low on gas driving on an abandoned road. Avoid obstacles and collect gas along the way to be able to make it to the city successfully.
+### `This vertical scrolling experience is about a vehicle low on gas driving on an abandoned road. Avoid obstacles and collect gas along the way to be able to make it to the city successfully.`
+ 
+## `To Play:`
+ 
+`Start By Pressing the Up Arrow Key`
+ 
+ `Move left and right along the road by pressing (not holding) the left and right arrow keys`
+ 
+ `Drive through gas cans to collect gas and avoid road hazards (yellow signs)`
+ 
+`Your current gas level is displayed in the bottom left of the screen. Your gas depletes 20% every 2 seconds`
+ 
+ `If your gas level reaches 0% or you drive into a road hazard, the game ends.`
+ 
+ 
+  `Have Fun!`
+ 
 - - -
-  `Credits to Vecteezy.com for gas can and gas meter images.`
+  Credits to Vecteezy.com for gas can, gas meter, gas pump, and light pole assets.
+ 
+Credits to Kenney.nl for car, road, rock, and foliage assets.
  */
 
 loadPrgm(true)
